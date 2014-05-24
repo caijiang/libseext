14e085014e86bbf0ba24bcd5776e97c678bada35
luffyj <luffy.ja@gmail.com>
2014-05-24 15:01:44 +0800

    init

--- src/main/java/org/luffy/libs/libseext/BytesUtils.java
+++ src/main/java/org/luffy/libs/libseext/BytesUtils.java
@@ -3,33 +3,45 @@
  * To change this template file, choose Tools | Templates
  * and open the template in the editor.
  */
-
 package org.luffy.libs.libseext;
+
+import java.security.MessageDigest;
+import java.security.NoSuchAlgorithmException;
 
 /**
  *
  * @author luffy
  */
 public class BytesUtils {
-    
-    public static String base64(byte[] src){
+
+    public static String base64(byte[] src) {
         return java.util.Base64.getEncoder().encodeToString(src);
     }
-    
-    public static byte[] base64(String src){
+
+    public static byte[] base64(String src) {
         return java.util.Base64.getDecoder().decode(src);
     }
-    
-    public static String byes2Str(byte[] data){
+
+    public static byte[] sha1(byte[] data) {
+        try {
+            MessageDigest messageDigest = MessageDigest.getInstance("sha1");
+            messageDigest.update(data);
+            return (messageDigest.digest());
+        } catch (NoSuchAlgorithmException e) {
+            throw new RuntimeException(e);
+        }
+    }
+
+    public static String byes2Str(byte[] data) {
         StringBuilder sb = new StringBuilder();
-        for (byte b:data) {
+        for (byte b : data) {
             String t = Integer.toUnsignedString(b, 16);
-            if(t.length()>=8){
+            if (t.length() >= 8) {
                 sb.append(t.substring(6));
-            }else if(t.length()<2){
+            } else if (t.length() < 2) {
                 sb.append('0');
                 sb.append(t);
-            }else{
+            } else {
                 sb.append(t);
             }
         }
--- src/main/java/org/luffy/libs/libseext/XMLUtils.java
+++ src/main/java/org/luffy/libs/libseext/XMLUtils.java
@@ -3,13 +3,20 @@
  * To change this template file, choose Tools | Templates
  * and open the template in the editor.
  */
-
 package org.luffy.libs.libseext;
 
 import java.io.IOException;
 import java.io.InputStream;
+import java.io.StringWriter;
 import javax.xml.parsers.DocumentBuilderFactory;
 import javax.xml.parsers.ParserConfigurationException;
+import javax.xml.transform.OutputKeys;
+import javax.xml.transform.Transformer;
+import javax.xml.transform.TransformerConfigurationException;
+import javax.xml.transform.TransformerException;
+import javax.xml.transform.TransformerFactory;
+import javax.xml.transform.dom.DOMSource;
+import javax.xml.transform.stream.StreamResult;
 import org.w3c.dom.Document;
 import org.xml.sax.SAXException;
 
@@ -18,8 +25,19 @@ import org.xml.sax.SAXException;
  * @author luffy
  */
 public class XMLUtils {
-    
+
     public static Document xml2doc(InputStream inputStream) throws IOException, SAXException, ParserConfigurationException {
         return DocumentBuilderFactory.newInstance().newDocumentBuilder().parse(inputStream);
+    }
+
+    public static String doc2str(Document doc) throws TransformerConfigurationException, TransformerException {
+        Transformer transformer = TransformerFactory.newInstance().newTransformer();
+        transformer.setOutputProperty(OutputKeys.INDENT, "yes");
+        //initialize StreamResult with File object to save to file
+        StreamResult result = new StreamResult(new StringWriter());
+        DOMSource source = new DOMSource(doc);
+        transformer.transform(source, result);
+        String xmlString = result.getWriter().toString();
+        return xmlString;
     }
 }
