3fe14f3354457e4e16766aef7f60964557dd032c
HZCJ <luffy.ja@gmail.com>
2015-08-03 18:43:41 +0800

    add Utils

--- src/main/java/org/luffy/libs/libseext/CollectionUtils.java
+++ src/main/java/org/luffy/libs/libseext/CollectionUtils.java
@@ -1,5 +1,6 @@
 package org.luffy.libs.libseext;
 
+import java.util.Collections;
 import java.util.HashSet;
 import java.util.Set;
 
@@ -11,14 +12,25 @@ import java.util.Set;
 public class CollectionUtils {
 
     /**
-     *
      * @param data 元素
-     * @param <T> 类型
+     * @param <T>  类型
      * @return HashSet实例，包含一个元素
      */
     public static <T> Set<T> singleSet(T data) {
         HashSet<T> set = new HashSet<>();
         set.add(data);
+        return set;
+    }
+
+    /**
+     *
+     * @param data 元素集合
+     * @param <T> 类型
+     * @return HashSet实例，包含所有元素
+     */
+    public static <T> Set<T> mutliSet(T... data) {
+        HashSet<T> set = new HashSet<>();
+        Collections.addAll(set, data);
         return set;
     }
 
